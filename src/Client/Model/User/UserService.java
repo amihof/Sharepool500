e0956cda9c48af34d137231138651d2df851ce86
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2022-04-14 15:22:39 +0200

    Imported Confirmation token

--- src/Client/Model/User/UserService.java
+++ src/Client/Model/User/UserService.java
@@ -1,6 +1,8 @@
 package Client.Model.User;
 
 
+import Client.Model.Registration.Token.ConfirmationToken;
+import Client.Model.Registration.Token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;

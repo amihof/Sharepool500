d84195515915146ae7c0e85ae5f8c0478e0e87df
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2022-04-18 10:30:57 +0200

    SQLQuery

--- src/Client/Model/Annons.java
+++ src/Client/Model/Annons.java
@@ -4,6 +4,7 @@ import java.io.Serial;
 import java.io.Serializable;
 
 public class Annons implements Serializable {
+
     @Serial
     private static final long serialVersionUID = 5403016831143700756L;
 
--- src/Server/Model/SQLquery.java
+++ src/Server/Model/SQLquery.java
@@ -1,6 +1,8 @@
 package Server.Model;
 
+import javax.xml.transform.Result;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class SQLquery {
 
@@ -50,7 +52,39 @@ public class SQLquery {
 
     }
 
-    public boolean search(String text, Enum<> category Date date){
+    public ArrayList<Annons> search(String text, Enum<Category> category, Date date){
+
+        Connection con = Server.getCon();
+        PreparedStatement pstmt = null;
+        try {
+            String QUERY = String.format("INSERT INTO \"user\" (username, email, password) " +
+                    "VALUES('%s', '%s', '%s');",username, email, password );
+
+            pstmt = con.prepareStatement(QUERY);
+            pstmt.setString(1, username);
+            pstmt.setString(2, email);
+            pstmt.setString(3, password);
+
+            ResultSet resultSet = pstmt.executeQuery();
+
+            ArrayList<Annons> result;
+            Annons tempAnnons;
+            while (resultSet.next()){
+                result.add(new Annons(resultSet.getString(1)));
+            }
+
+
+
+            return pstmt.execute();
+
+        } catch (Exception p) {
+            return null;
+        }
+
+
+    }
+
+    public boolean createAnnons(String text){
 
         Connection con = Server.getCon();
         PreparedStatement pstmt = null;

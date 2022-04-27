deaefd42902a68e5d1b4b1981a3951ec720284ea
amihof <amidala.hoffmen@live.se>
2022-04-27 10:37:22 +0200

    uppdaterat mainframe

--- src/Client/View/Main/MainFrame.java
+++ src/Client/View/Main/MainFrame.java
@@ -71,18 +71,6 @@ public class MainFrame
         frame.setVisible(true);
 
         this.controller = controller;
-       /* this.mainPanelSFD = new MainPanelSFD(this.width, this.height, controller, loggedIn);
-        this.mainPanelMinaSidor = new MainPanelMinaSidor(width, height, controller, loggedIn);
-        this.mainPanelAnnons = new MainPanelAnnons(width, height, controller, loggedIn);
-        this.mainPanelSkapaAnnons = new MainPanelSkapaAnnons(width, height, controller, loggedIn);
-        this.frame = new JFrame();
-        this.mainPanel = new MainPanel(this.width, this.height, controller, loggedIn);
-        this.frame.setPreferredSize(new Dimension(this.width, this.height));
-        this.frame.setResizable(true);
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.pack();
-        this.frame.setVisible(true);
-        this.frame.add((Component)this.mainPanel);*/
     }
 
     public void loginButtonClicked(final Controller controller) {
@@ -91,99 +79,6 @@ public class MainFrame
 
     public void andraUppgifterClicked(final Controller controller) {
         this.redigeraUppgifter = new RedigeraUppgifter(controller);
-    }
-
-    public void clearJFrame(final Controller controller) {
-        final Component[] components = this.mainPanel.getComponents();
-        final Component[] components2 = this.mainPanelSFD.getComponents();
-        final Component[] components3 = this.mainPanelMinaSidor.getComponents();
-        final Component[] components4 = this.mainPanelAnnons.getComponents();
-
-        for (final Component component : components) {
-            this.mainPanel.remove(component);
-        }
-        for (final Component component : components2) {
-            this.mainPanelSFD.remove(component);
-        }
-        for (final Component component : components3) {
-            this.mainPanelMinaSidor.remove(component);
-        }
-        for (final Component component : components4) {
-            this.mainPanelAnnons.remove(component);
-        }
-    }
-
-    public void updateJFrame(Controller controller) {
-        this.mainPanelSFD = new MainPanelSFD(width, height, controller, loggedIn);
-        this.frame.add(this.mainPanelSFD);
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.setPreferredSize(new Dimension(this.width, this.height));
-        this.frame.setResizable(true);
-        this.frame.pack();
-        this.frame.setVisible(true);
-        this.frame.setContentPane(this.mainPanelSFD);
-        this.frame.revalidate();
-        this.frame.repaint();
-    }
-
-    public void updateJFrameHome(Controller controller) {
-        this.mainPanel = new MainPanel(width, height, controller,loggedIn);
-        this.frame.add(this.mainPanel);
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.setPreferredSize(new Dimension(this.width, this.height));
-        this.frame.setResizable(true);
-        this.frame.pack();
-        this.frame.setVisible(true);
-        this.frame.setContentPane(this.mainPanel);
-        this.frame.revalidate();
-        this.frame.repaint();
-    }
-
-    public void updateJFrameMinaSidor(Controller controller) {
-        this.mainPanelMinaSidor = new MainPanelMinaSidor(width, height, controller, loggedIn);
-        this.frame.add(this.mainPanelMinaSidor);
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.setPreferredSize(new Dimension(this.width, this.height));
-        this.frame.setResizable(true);
-        this.frame.pack();
-        this.frame.setVisible(true);
-        this.frame.setContentPane(this.mainPanelMinaSidor);
-        this.frame.revalidate();
-        this.frame.repaint();
-    }
-
-    public void updateJFrameSkapaAnnons(Controller controller) {
-        topPanel = new TopPanel(width, height, controller, "0", loggedIn);
-       // this.frame.add(this.mainPanelSkapaAnnons);
-        frame.add(topPanel);
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.setPreferredSize(new Dimension(this.width, this.height));
-        this.frame.setResizable(true);
-        this.frame.pack();
-        this.frame.setVisible(true);
-        this.frame.setContentPane(topPanel);
-        addScroll();
-        this.frame.revalidate();
-        this.frame.repaint();
-
-    }
-
-    public void updateJFrameAnnons(Controller controller) {
-        topPanel = new TopPanel(width, height, controller, "0", loggedIn);
-        annonsPanel = new AnnonsPanel(width, height-100, controller);
-        //this.mainPanelAnnons = new MainPanelAnnons(width, height, controller);
-        //this.frame.add(this.mainPanelAnnons);
-        frame.add(topPanel);
-        this.frame.setDefaultCloseOperation(3);
-        this.frame.setPreferredSize(new Dimension(this.width, this.height));
-        this.frame.setResizable(true);
-        this.frame.pack();
-        this.frame.setVisible(true);
-        this.frame.setContentPane(topPanel);
-        frame.add(annonsPanel);
-        addAnnonserPanel();
-        this.frame.revalidate();
-        this.frame.repaint();
     }
 
     public void addScroll() {
@@ -199,23 +94,6 @@ public class MainFrame
         scrollPanel.setBounds(0, 0, width, height);
         scrollPanel.setBorder(BorderFactory.createEmptyBorder());
         scrollPanel.add(topPanel);
-    }
-
-    public void addAnnonserPanel(){
-        this.listContainer = new JPanel();
-        listContainer.setLayout(new BoxLayout(listContainer, BoxLayout.Y_AXIS));
-
-        final JScrollPane scrollPanel = new JScrollPane(
-                listContainer,
-                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
-                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
-        );
-        listContainer.setPreferredSize(new Dimension((int) listContainer.getPreferredSize().getWidth(),
-                (int)(listContainer.getPreferredSize().getHeight()+500)));
-        scrollPanel.setBounds(0, 250, width, height-100);
-        scrollPanel.setBorder(BorderFactory.createEmptyBorder());
-        frame.add(scrollPanel);
-        frame.setVisible(true);
     }
 
     public void addAnnons(){

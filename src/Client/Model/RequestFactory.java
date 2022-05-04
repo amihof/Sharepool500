7f7985d60bf10607914ffe732746872bc6a970bd
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2022-05-04 13:15:30 +0200

    comment in request factory

--- src/Client/Model/RequestFactory.java
+++ src/Client/Model/RequestFactory.java
@@ -6,19 +6,21 @@ import Delad.User;
 
 import java.util.ArrayList;
 
+/**
+ * The request factory is responsible for constructing request to be sent to the server.
+ * The class mediated the connection from the controller to client class*/
 public class RequestFactory {
     private Client client;
 
     /**@constructor
-     * @param client represents each client **/
+     * @param client represents the current client **/
     public RequestFactory(Client client){
         this.client = client;
     }
 
-    /**When the user wants to log in a new request is created
+    /**This function creates new request is created
      * and added to the inputBuffer-buffer in Client-class
      * @param user represents each user**/
-
     public void login(User user){
         client.addToBuffer(new Request(false, user));
     }
